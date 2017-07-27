baa31faa01dc4f5f8d5b774def7813f8fe55cbee
bamodio <bamodio@users.noreply.github.com>
2017-07-27 06:48:45 -0700

    Updating version to now produced a signed Nuget package version

--- build/AssemblyInfo.cs
+++ build/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rack")]
 [assembly: AssemblyProduct("Rack.Assertions")]
-[assembly: AssemblyCopyright("Copyright Â©2017 Ben Amodio")]
+[assembly: AssemblyCopyright("Copyright ©2017 Ben Amodio")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,7 +23,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+
 
 
 
